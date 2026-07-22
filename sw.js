e97b461705064bcde45e206b1ac6b94e78e1c252
sahni4you <sahni4you@yahoo.com>
2026-07-22 07:56:52 +0530

    Add files via upload
    sw.js uploaded

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'shorts-player-v1';
+
+// Files to cache for offline access
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Minimal fetch listener required by Chrome for PWA installability
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
